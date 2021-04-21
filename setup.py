017b66b6c8ddbc76073b2b34fd37256e26719f91
Hendrik Kleikamp <h_klei18@uni-muenster.de>
2021-04-21 09:49:13 +0200

    initial commit including CI setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='feature-extraction',
+    version='0.1.0',
+    description='Algorithms for image feature extraction',
+    author='Hendrik Kleikamp',
+    maintainer='Hendrik Kleikamp',
+    maintainer_email='hendrik.kleikamp@uni-muenster.de',
+    packages=['feature_extraction', ],
+)
